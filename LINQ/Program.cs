4e550cfcb47e92eb464227b21590c8317ccbb766
Victoria Pham <victoriapham52@gmail.com>
2023-03-28 14:20:42 -0700

    Exercise Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Create a list of video game names...
+            Order the list of games by length of the game name.
+            Use the lambda expression in this exercise as well.
+            use Method Syntax for this exercise*/
+
+            var movieList = new List<string>
+            { "First Wives Club",
+                "Birdcage",
+                "Mrs.Doubtfire",
+                "My Cousin Vinny"
+            };
+
+            var sortedMovies = movieList.OrderBy(movie => movie.Length);
+
+            foreach (var movie in sortedMovies)
+            {
+                Console.WriteLine(movie);
+            }
+
         }
     }
 }
